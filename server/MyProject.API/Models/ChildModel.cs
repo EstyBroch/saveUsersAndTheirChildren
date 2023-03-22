3d7deeb78bfad062689bcc794a248c89e31cfabb
esty broch <brochesty@gmail.com>
2023-03-22 21:09:10 +0200

    first commit

---
+++ server/MyProject.API/Models/ChildModel.cs
@@ -0,0 +1,16 @@
+﻿using MyProject.Repositories.Entities;
+using System;
+
+namespace MyProject.WebAPI.Models
+{
+    public class ChildModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string IdNumber { get; set; }
+        public int? IdFather { get; set; }
+        public int? IdMother { get; set; }
+      
+    }
+}
